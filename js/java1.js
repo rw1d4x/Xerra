a17411b422ac3296db3039338a6c8f95857aa485
rw1d4x <2204982@talnet.nl>
2025-01-05 14:45:28 +0100

    Update java1.js
    update java1.js nieuwste versie 5/1 14:45

--- js/java1.js
+++ js/java1.js
@@ -1,1 +1,19 @@
-// nog niks ;(
+/*w3schools heeft mij hier volledig bij geholpen*/
+
+let mybutton = document.getElementById("knop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
